7af1d787fbd065b525be03fe85cf967b5aa0f4b5
ayandamkula <ayanda@opencartplugins.net>
2021-10-22 15:46:28 +0200

    Added service account file and test web app

---
+++ react-app/src/OtherPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const body = () => { return (
+  <div>
+    Im some other page!
+    <Link to="/">Go back home</Link>
+  </div>
+); };
+export default body;
